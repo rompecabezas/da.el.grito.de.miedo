0ed16fe1c5e657c656ec3b43104fe20f567038ab
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2015-09-14 03:05:51 -0500

    Creo un pequeño socket con angular y socket.io, con el que enviaré al servidor los datos de los participantes.

---
+++ js/angular-socket-io-submit.js
@@ -0,0 +1,46 @@
+angular.module('grita con creepypastas').factory('socket',['$rootScope', function ($rootScope) {
+  var socket = io.connect('http://199.89.53.200:9129', {
+      'reconnection delay': 2000,
+      'force new connection': true
+  });
+
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+}]);
+
+angular.module('grita con creepypastas').controller('nlform-controller', ['$scope', 'socket', function($scope,socket) {
+  socket.on('connect', function() {
+      var event = {
+          body: "User arrives"
+      };
+      socket.emit('user::arrives', event);
+  });
+
+  socket.on('user::arrives', function(data) {
+    console.log(data);
+  });
+
+
+  $scope.send = function(){
+    alert(':D');
+  }
+
+}]);
